bd67e02c7009243fabb509530d71f93f1ac4bb30
HanBee Jang <hanbee990508@naver.com>
2016-06-02 20:57:02 +0900

    #2 JAVA PROGRAMMING

--- app/src/main/java/kr/hs/emirim/hanbee990508/simplewebbrowser/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/hanbee990508/simplewebbrowser/MainActivity.java
@@ -3,33 +3,62 @@ package kr.hs.emirim.hanbee990508.simplewebbrowser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.text.BreakIterator;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText editUrl;
+    Button butMove, butPrev;
+    WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        EditText enditUrl;
-        Button butMove, butPrev;
-        WebView web;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        enditUrl=(EditText)findViewById(R.id.edit_url);
-        butMove=(Button)findViewById(R.id.but_move);
-        butMove=(Button)findViewById(R.id.but_prev);
-        web=(WebView)findViewById(R.id.web);
+        editUrl = (EditText) findViewById(R.id.edit_url);
+        butMove = (Button) findViewById(R.id.but_move);
+        butPrev = (Button) findViewById(R.id.but_prev);
+        web = (WebView) findViewById(R.id.web);
         butMove.setOnClickListener(this);
+        butPrev.setOnClickListener(this);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+        webSet.setJavaScriptEnabled(true);
+        //
+        //
+        // web.loadUrl("http://www.naver.com");
+
+
     }
 
     /**
      * Called when a view has been clicked.
+     * ß
      *
      * @param v The view that was clicked.
      */
     @Override
     public void onClick(View v) {
+        String url = null;
+        switch (v.getId()){
+            case R.id.but_move:
+                url=editUrl.getText().toString();
+                if(!url.contains("http"))
+                    url="http://"+url;
+                web.loadUrl(url);
+                break;
+            case R.id.but_prev:
+                web.goBack();
+                break;
+        }
 
     }
 }
+
